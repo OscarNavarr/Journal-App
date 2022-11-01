cb6d0ed79e2ffbf5d0d330b21af05e46dc5badc5
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-11-01 08:40:12 +0100

    Register page finished, i'm work at home page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,21 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-      <DeleteIcon/>
+    <JournalLayout>
 
-    </>
+      <Typography>
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque quis ullam libero iste cum explicabo, quod vel, ex praesentium velit tempora inventore nulla ducimus reprehenderit sapiente provident dicta illo.
+
+      </Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
  
   )
 }
