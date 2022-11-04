80c7ea0362ce85418595fa88944bcb6b60a073e0
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-11-04 21:32:04 +0100

    The Home Page was modified

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,16 +2,19 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>
+      
+      <NothingSelectedView/>
+      {/*<Typography>
 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque quis ullam libero iste cum explicabo, quod vel, ex praesentium velit tempora inventore nulla ducimus reprehenderit sapiente provident dicta illo.
 
-      </Typography>
+      </Typography>*/}
 
       {/* NothingSelected */}
       {/* NoteView */}
