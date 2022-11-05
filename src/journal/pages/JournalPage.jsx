0c40104cfd9d4f8094546c29c6b1bd41f7659eb4
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-11-05 12:16:48 +0100

    JournalApp's frontend was finished

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,30 @@
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Typography } from '@mui/material'
-import React from 'react'
+import { IconButton, Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
       
+      {/*<NoteView/>*/}
       <NothingSelectedView/>
+      
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor:'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom:50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
       {/*<Typography>
 
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cumque quis ullam libero iste cum explicabo, quod vel, ex praesentium velit tempora inventore nulla ducimus reprehenderit sapiente provident dicta illo.
