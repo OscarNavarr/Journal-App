eb85852b7c593672a3bbef892ab21bbfab51adaf
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-01-12 23:21:00 +0100

    Working on register page backend side

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -24,5 +24,24 @@ export const signInWithGoogle = async() => {
             ok: false,
             errorMessage,
         }
+    }
+}
+
+export const registerUserWithEmailPassword = async({ email, password, displayName }) => {
+    try {
+
+        const resp = await createUserWithEmailAndPassword( FirebaseAuth, email, password);
+        const {uid, photoURL } = resp.user;
+        console.log(resp);
+        //TODO: actualizar el displayName en Firebase
+
+        return{
+            ok:true,
+            uid, photoURL, email, displayName
+        }
+
+    } catch (error) {
+        console.log(error);
+        return {ok:false, errorMessage:error.message}
     }
 }
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
-import { signInWithGoogle } from "../../firebase/providers";
+
+import { registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./authSlice"
 
 export const checkingAuthentication = ( email, password ) => {
@@ -16,5 +17,14 @@ export const startGoogleSignIn = () => {
         if( !result.ok ) return dispatch( logout( result.errorMessage) );
         dispatch( login(result));
 
+    }
+}
+export const startCreatingUserWithEmailPassword = ({ email, password, displayName }) => {
+    return async( dispatch) => {
+        dispatch( checkingCredentials());
+
+        const resp = await registerUserWithEmailPassword({ email, password, displayName });
+
+        console.log(resp);
     }
 }
