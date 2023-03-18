9a5a561253f905303be4c88338c888cea429c9c4
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-03-18 22:51:54 +0100

    Created checking Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { CheckingAuth } from '../ui/index'
 
 export const AppRouter = () => {
+
+  const { status } = useSelector(state => state.auth);
+    return <CheckingAuth/>
   return (
     <Routes>
         {/* Login y Registro */}
